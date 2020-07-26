9f5d50c4891aac011999a5b8b79ad6c5104a4064
tobor <rosborne@osbornepro.com>
2020-07-25 22:13:24 -0600

    Initial commit of Find-CVE-2020-1350.ps1 and adds examples to Enable-DoH

--- Enable-DoH.ps1
+++ Enable-DoH.ps1
@@ -11,6 +11,20 @@
     If this parameter is defined the executioner will be prompted to restart the computer to fully apply the change.
 
 
+.EXAMPLE
+    -------------------------- EXAMPLE 1 --------------------------
+    C:\PS> Enable-DoH
+    This example enables DNS over HTPS on a windows machine.
+
+    -------------------------- EXAMPLE 2 --------------------------
+   C:\PS> Enable-DoH -Restart
+   This example enables DNS over HTTPS on a Windows machines and then restarts the computer after the runner confirms
+
+   -------------------------- EXAMPLE 3 --------------------------
+   C:\PS> Enable-DoH -Undo -Restart
+   This example disable DNS over HTTPS on a Windows machine and restarts the computer after the runner confirms
+
+
 .NOTES
     Author: Robert H. Osborne
     Alias: tobor
@@ -19,8 +33,10 @@
 
 .LINK
     https://github.com/tobor88
+    https://gitlab.com/tobor88
     https://www.powershellgallery.com/profiles/tobor
     https://roberthosborne.com
+    https://osbornepro.com
 
 
 .INPUTS
